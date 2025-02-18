b581257eb95a28e74e4b1004359e8122deebf846
Roger D. Peng <rdpeng@gmail.com>
2025-02-18 08:42:53 -0600

    Update PARD data

--- dates_times.R
+++ dates_times.R
@@ -5,11 +5,11 @@
 ## Austin 311 data from https://data.austintexas.gov
 
 
-## Need to load tidyverse AND lubridate
+## Need to load tidyverse
 library(tidyverse)
 
 ## Read the data
-dat <- read_csv("PARD_311_Data.csv.gz")
+dat <- read_csv("PARD_311_Data_20250218.csv.bz2")
 dat
 
 ## Quick look
@@ -18,11 +18,9 @@ glimpse(dat)
 ## What kinds of service requests are there?
 table(dat$`SR Description`)
 
-table(dat$`SR Description`)
-
 ## Make a bar plot of the SR Description variable
-dat |> 
-    ggplot(aes(x = `SR Description`)) + 
+dat |>
+    ggplot(aes(x = `SR Description`)) +
     geom_bar() +
     coord_flip()
 
@@ -30,21 +28,21 @@ glimpse(dat)
 
 
 ## Convert date variables into R date format
-dat <- dat |> 
+dat <- dat |>
     mutate(datetime = mdy_hms(`Created Date`, tz = ""),
            close = mdy_hms(`Close Date`, tz = ""))
 
 ## Look at newly created variables
-dat |> 
+dat |>
     select(`Created Date`, datetime, close)
 
 
 ## Show total number of service requests by month
 dat |>
-    mutate(month = month(datetime)) |> 
-    group_by(month) |> 
-    summarize(n = n()) |> 
-    ggplot(aes(x = month, y = n)) + 
+    mutate(month = month(datetime)) |>
+    group_by(month) |>
+    summarize(n = n()) |>
+    ggplot(aes(x = month, y = n)) +
     geom_point() +
     geom_line() +
     scale_x_continuous(breaks = 1:12, labels = month.name)
@@ -54,65 +52,65 @@ month.abb
 ## Show total number of service requests by month and year
 dat |>
     mutate(month = month(datetime),
-           year = year(datetime)) |> 
-    group_by(year, month) |> 
-    summarize(n = n()) |> 
-    ggplot(aes(x = month, y = n)) + 
+           year = year(datetime)) |>
+    group_by(year, month) |>
+    summarize(n = n()) |>
+    ggplot(aes(x = month, y = n)) +
     geom_point() +
     geom_line() +
     facet_wrap(vars(year)) +
     scale_x_continuous(breaks = 1:12)
 
 ## Distribution of time to close a service request
-dat |> 
+dat |>
     mutate(date = as.Date(datetime),
-           closedate = as.Date(close)) |> 
-    select(date, closedate) |> 
-    mutate(timetoclose = as.integer(closedate - date)) |> 
-    select(timetoclose) |> 
+           closedate = as.Date(close)) |>
+    select(date, closedate) |>
+    mutate(timetoclose = as.integer(closedate - date)) |>
+    select(timetoclose) |>
     summary()
 
 ## Plot distribution of time to close a service request
-dat |> 
+dat |>
     mutate(date = as.Date(datetime),
-           closedate = as.Date(close)) |> 
-    select(date, closedate) |> 
-    mutate(timetoclose = as.integer(closedate - date)) |> 
-    select(timetoclose) |> 
-    filter(!is.na(timetoclose)) |> 
-    ggplot(aes(x = timetoclose)) + 
+           closedate = as.Date(close)) |>
+    select(date, closedate) |>
+    mutate(timetoclose = as.integer(closedate - date)) |>
+    select(timetoclose) |>
+    filter(!is.na(timetoclose)) |>
+    ggplot(aes(x = timetoclose)) +
     geom_histogram(bins = 20)
 
 ## Look at times less than 60 days
-dat |> 
+dat |>
     mutate(date = as.Date(datetime),
-           closedate = as.Date(close)) |> 
-    select(date, closedate) |> 
-    mutate(timetoclose = as.integer(closedate - date)) |> 
-    filter(!is.na(timetoclose)) |> 
-    filter(timetoclose < 60) |> 
-    ggplot(aes(x = timetoclose)) + 
+           closedate = as.Date(close)) |>
+    select(date, closedate) |>
+    mutate(timetoclose = as.integer(closedate - date)) |>
+    filter(!is.na(timetoclose)) |>
+    filter(timetoclose < 60) |>
+    ggplot(aes(x = timetoclose)) +
     geom_histogram(bins = 20)
 
 ## Show daily number of service requests
-dat |> 
-    mutate(date = as.Date(datetime)) |> 
-    group_by(date) |> 
-    summarize(n = n()) |> 
-    arrange(date) |> 
-    ggplot(aes(x = date, y = n)) + 
+dat |>
+    mutate(date = as.Date(datetime)) |>
+    group_by(date) |>
+    summarize(n = n()) |>
+    arrange(date) |>
+    ggplot(aes(x = date, y = n)) +
     geom_line()
 
 
 ## Show daily number of service requests by ZIP code
-dat |> 
-    mutate(date = as.Date(datetime)) |> 
-    rename(zipcode = `Zip Code`) |> 
-    group_by(zipcode, date) |> 
-    summarize(n = n()) |> 
-    arrange(date) |> 
-    ggplot(aes(x = date, y = n)) + 
-    geom_point() + 
+dat |>
+    mutate(date = as.Date(datetime)) |>
+    rename(zipcode = `Zip Code`) |>
+    group_by(zipcode, date) |>
+    summarize(n = n()) |>
+    arrange(date) |>
+    ggplot(aes(x = date, y = n)) +
+    geom_point() +
     facet_wrap(vars(zipcode))
 
 
@@ -122,7 +120,7 @@ dat |>
 competitions <- read_tsv("WCA_export_Competitions.tsv.bz2")
 competitions
 
-competitions |> 
+competitions |>
     select(year, month, day, endMonth, endDay)
 
 
